edd3dea5f7f35d36b71dd35efbd5778c43a36e8a
MohammadYounusM <mohammadyounusafghan16@gmail.com>
2024-11-24 06:56:01 -0800

    adding pages  addresses

---
+++ lib/Pages_Address.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_3/main.dart';
+
+class addresses extends StatelessWidget {
+  const addresses({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: 
+      "/",
+      routes: {
+        "/": (context) => home(),
+        // "first_item": (context) => 
+      },
+      
+    );
+  }
+}
